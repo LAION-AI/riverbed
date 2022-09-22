8f647e4ab1980f2daa11db8a21d98d8e833014c3
ontocord <ontocord@gmail.com>
2022-09-21 19:07:39 -0700

    Update riverbed.py

--- riverbed.py
+++ riverbed.py
@@ -53,6 +53,8 @@ from fast_pytorch_kmeans import KMeans
 import torch
 import tqdm
 import gzip
+import multiprocessing
+
 if torch.cuda.is_available():
   device = 'cuda'
 else:
@@ -123,33 +125,62 @@ class RiverbedTokenizer:
 
   def token2idx(self):
     return OrderedDict([(term, idx) for idx, term in enumerate(self.tokenweight.items())])
-    
-  def tokenize(self, doc, min_compound_weight=0,  max_compound_word_size=10000, compound=None, token2weight=None, synonyms=None, use_synonym_replacement=False):
+  
+  @staticmethod
+  def _tokenize(doc_batch, min_compound_weight=0,  max_compound_word_size=10000, compound=None, token2weight=None, synonyms=None, use_synonym_replacement=False, return_str=True):
+    if not use_synonym_replacement: synonyms = {} 
+    ret = []
+    for doc in doc_batch:
+      doc = [synonyms.get(d,d) for d in doc.split(" ") if d.strip()]
+      len_doc = len(doc)
+      for i in range(len_doc-1):
+          if doc[i] is None: continue
+          tokenArr = doc[i].strip("_").replace("__", "_").split("_")
+          if tokenArr[0] in compound:
+            min_compound_len = compound[tokenArr[0]][0]
+            max_compound_len = min(max_compound_word_size, compound[tokenArr[0]][-1])
+            for j in range(min(len_doc, i+max_compound_len), i+1, -1):
+              if j <= i+min_compound_len-1: break
+              token = ("_".join(doc[i:j])).strip("_").replace("__", "_")
+              tokenArr = token.split("_")
+              if len(tokenArr) <= max_compound_len and token in token2weight and token2weight.get(token, 0) >= min_compound_weight:
+                old_token = token
+                doc[j-1] = synonyms.get(token, token).strip("_").replace("__", "_")
+                #if old_token != doc[j-1]: print (old_token, doc[j-1])
+                for k in range(i, j-1):
+                    doc[k] = None
+                break
+      if return_str: 
+        ret.append (" ".join([d for d in doc if d]))
+     else: 
+      ret.append([d for d in doc if d])
+     return ret
+  
+  def tokenize(self, doc_batch, min_compound_weight=0,  max_compound_word_size=10000, compound=None, token2weight=None, synonyms=None, use_synonym_replacement=False, return_str=True):
     if synonyms is None: synonyms = {} if not hasattr(self, 'synonyms') else self.synonyms
     if token2weight is None: token2weight = {} if not hasattr(self, 'token2weight') else self.token2weight    
     if compound is None: compound = {} if not hasattr(self, 'compound') else self.compound
-    if not use_synonym_replacement: synonyms = {} 
-    doc = [synonyms.get(d,d) for d in doc.split(" ") if d.strip()]
-    len_doc = len(doc)
-    for i in range(len_doc-1):
-        if doc[i] is None: continue
-        tokenArr = doc[i].strip("_").replace("__", "_").split("_")
-        if tokenArr[0] in compound:
-          min_compound_len = compound[tokenArr[0]][0]
-          max_compound_len = min(max_compound_word_size, compound[tokenArr[0]][-1])
-          for j in range(min(len_doc, i+max_compound_len), i+1, -1):
-            if j <= i+min_compound_len-1: break
-            token = ("_".join(doc[i:j])).strip("_").replace("__", "_")
-            tokenArr = token.split("_")
-            if len(tokenArr) <= max_compound_len and token in token2weight and token2weight.get(token, 0) >= min_compound_weight:
-              old_token = token
-              doc[j-1] = synonyms.get(token, token).strip("_").replace("__", "_")
-              #if old_token != doc[j-1]: print (old_token, doc[j-1])
-              for k in range(i, j-1):
-                  doc[k] = None
-              break
-    return (" ".join([d for d in doc if d]))
-
+    if type(doc_batch) is str:
+      doc_batch = [doc_batch]
+    if len(doc_batch) < 1000:
+     ret = tokenize(doc_batch, min_compound_weight=min_compound_weight,  max_compound_word_size=max_compound_word_size, \
+                     compound=compound, token2weight=token2weight, synonyms=synonyms, use_synonym_replacement=use_synonym_replacement, \
+                     return_str=return_str)
+     if len(ret) == 1:
+      return ret[0]
+     else:
+      return ret
+    else:
+      chunk_size = int(len(doc_batch)/multiprocessing.cpu_count())
+      pool = multiprocessing.Pool(processes=multiprocessing.cpu_count()) 
+      ret = pool.imap(partial(Riverbed._tokenize, min_compound_weight=min_compound_weight,  max_compound_word_size=max_compound_word_size, \
+                                  compound=compound, token2weight=token2weight, synonyms=synonyms, use_synonym_replacement=use_synonym_replacement, \
+                                  return_str=return_str),
+                                  doc_batch, chunk_size)
+      return list(ret)
+    
+      
+      
   def save_pretrained(self, tokenizer_name):
       os.system(f"mkdir -p {tokenizer_name}")
       pickle.dump(self, open(f"{tokenizer_name}/{tokenizer_name}.pickle", "wb"))
