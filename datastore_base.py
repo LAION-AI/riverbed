a532f6a8a980360a7c6ac672a0240a699eebafc9
ontocord <ontocord@gmail.com>
2022-10-08 09:25:02 -0700

    Update datastore_base.py

--- datastore_base.py
+++ datastore_base.py
@@ -1,4 +1,4 @@
-# Copyright 2021, Ontocord, LLC
+# Copyright 2021-2022, Ontocord, LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -88,7 +88,7 @@ sys.path.append(
 )
 
 
-from datastore2sql import *
+from .datastore2sql import *
 
 
 ######################################################################################
