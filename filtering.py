83c65ee2732dab4ebb85b16b0f0f1d79b42aa207
ontocord <ontocord@gmail.com>
2022-11-26 09:03:39 -0800

    Update filtering.py

--- filtering.py
+++ filtering.py
@@ -13,50 +13,46 @@ limitations under the License.
 """
 
 #adapted from https://github.com/piisa/muliwai/blob/main/preprocess_manager.py, and
-#https://github.com/piisa/muliwai/blob/7ccf36d016fa66a9b1ed00b0ce8b89d01a57dfc9/langid_manager.py which are under Apache 2.0
 
 from .stopwords import all_stopwords
+from .flaggedwords import flagged_words
 from .char_manager import *
-import fasttext, langid
-import os
-fasttext_model = os.path.abspath(os.path.dirname(__file__))  +"/bin/lid.176.ftz"
-lang_model = fasttext.load_model(fasttext_model)
 
 from collections import Counter
-def get_ngram(sent, window_size=3, lang="en"):
+def get_ngram(text, window_size=3, lang="en"):
   if lang in {"zh", "ja", "ko", "th"}:
-    tokens = sent
+    tokens = text
     ret= ["".join(tokens[i : i + window_size])   for i in range(len(tokens) - window_size)]
   else:
-    tokens = sent.split()
+    tokens = text.split()
   ret= [" ".join(tokens[i : i + window_size])   for i in range(len(tokens) - window_size)]
   return Counter(ret)
 
-def get_ngram_score(sent, window_size=3, lang="en"):
-  aHash = get_ngram(sent, window_size, lang)
-  sent_len = sent.count(" ")+1
+def get_ngram_score(lang, text, window_size=3):
+  aHash = get_ngram(text, window_size, lang)
+  text_len = text.count(" ")+1
   for key in list(aHash.keys()):
-    aHash[key] = aHash[key]/sent_len
+    aHash[key] = aHash[key]/text_len
   return aHash.most_common()[1]
   
-def get_special_char_score (text, special_characters_default=None):
+def get_special_char_score (lang, text, special_characters_default=None):
   global junk
+  #TODO: do we want to do any lang specific special_chars?
   if special_characters_default is None: special_characters_default = junk
   return len([a for a in text if a in special_characters_default])/len(text)
 
 
-    
 lang_2_max_stopword_len = dict([(lang, max(s.count(" ")+1 if lang not in {'zh', 'zh-classical', 'zh-min-nan', 'zh-yue', 'ko', 'ja', 'th', 'jv'} else len(s) for s in arr)) for lang, arr in all_stopwords.items()])
 
-def get_stopword_score(lang, doc, max_word_len=3, cjk_scale=1.5):
+def get_stopword_score(lang, text, max_word_len=3, cjk_scale=1.5):
     is_cjk = lang in {'zh', 'zh-classical', 'zh-min-nan', 'zh-yue', 'ko', 'ja', 'th', 'jv'}
     stopwords =  all_stopwords.get(lang, {})
     if not stopwords: return 1
-    doc = doc.lower().strip()
+    text = text.lower().strip()
     if is_cjk: 
-      s_arr = list("".join(doc.split())) 
+      s_arr = list("".join(text.split())) 
     else: 
-      s_arr = doc.split()
+      s_arr = text.split()
     word_len = lang_2_max_stopword_len.get(lang, max_word_len)
     len_s = len(s_arr)
     stop_cnt = 0
@@ -76,32 +72,63 @@ def get_stopword_score(lang, doc, max_word_len=3, cjk_scale=1.5):
     if is_cjk: stopword_score = stopword_score*cjk_scale
     return (stopword_score)
 
-def get_score_moving_avg(lang, text, scores_per_lang, simple_moving_avg_window=10, fn=None):
+
+    
+lang_2_max_flaggedword_len = dict([(lang, max(s.count(" ")+1 if lang not in {'zh', 'zh-classical', 'zh-min-nan', 'zh-yue', 'ko', 'ja', 'th', 'jv'} else len(s) for s in arr)) for lang, arr in flagged_words.items()])
+
+def get_flaggedword_score(lang, text, max_word_len=3, cjk_scale=1.5):
+    is_cjk = lang in {'zh', 'zh-classical', 'zh-min-nan', 'zh-yue', 'ko', 'ja', 'th', 'jv'}
+    flaggedwords =  flagged_words.get(lang, {})
+    if not flaggedwords: return 1
+    text = text.lower().strip()
+    if is_cjk: 
+      s_arr = list("".join(text.split())) 
+    else: 
+      s_arr = text.split()
+    word_len = lang_2_max_flaggedword_len.get(lang, max_word_len)
+    len_s = len(s_arr)
+    stop_cnt = 0
+    total_cnt = 0
+    for i in range(len_s):
+      if s_arr[i] is None: continue
+      for j in range(min(len_s, i+word_len), i, -1):
+        word = "".join(s_arr[i:j]) if is_cjk else " ".join(s_arr[i:j])
+        if word in flaggedwords:
+          stop_cnt += 1
+          s_arr[i] = "".join(s_arr[i:j]) if is_cjk else " ".join(s_arr[i:j]) 
+          for k in range(i+1, j):
+            s_arr[k] = None
+          break
+      total_cnt += 1
+    flaggedword_score =  (stop_cnt/total_cnt) 
+    if is_cjk: flaggedword_score = flaggedword_score*cjk_scale
+    return (flaggedword_score)
+
+
+def get_score_moving_avg(lang, text, scores_per_lang=None, _stdev_lower_bound=2, _stdev_upper_bound=2, simple_moving_avg_window=500, fn=None, fn_args={}, use_mean_for_cutoff=True):
     if fn is None: fn = get_stopword_score
-    _min_cutoff = _stdev = _mean = _median = 0
-    _score = fn(lang, text)
+    if scores_per_lang is None: scores_per_lang = {}
+    _max_cutoff = 10e9
+    _min_cutoff = -10e9
+    _stdev = _mean = _median = 0
+    _score = fn(lang, text, **fn_args)
     _min_cutoff = _mean = _median = _stdev = _quantiles = None
     scores_per_lang[lang] = _scores = _scores_per_lang.get(lang,[])
     _scores.append(_score)
-    if len(_scores) < 2:
-      _min_cutoff = _score
-    else:
+    if len(_scores) >= 2:
       if len(_scores) > simple_moving_avg_window:
-         _scores_per_lang[lang] = _scores=_scores[-simple_moving_avg_window:]
+         _scores_per_lang[lang] = _scores =_scores[-simple_moving_avg_window:]
       _stdev = statistics.stdev(_scores)
       _mean = statistics.mean (_scores)        
-      _median = statistics.median (_scores)        
-      _min_cutoff = _mean-(_stdev*_stdev_lower_bound)
-    return _min_cutoff, _stdev, _mean, _median 
-        
-import re
+      _median = statistics.median (_scores) 
+      if use_mean_for_cutoff:
+        _min_cutoff = _mean-(_stdev*_stdev_lower_bound)
+        _max_cutoff = _mean+(_stdev*_stdev_upper_bound)
+      else:
+        _min_cutoff = _median-(_stdev*_stdev_lower_bound)
+        _max_cutoff = _median+(_stdev*_stdev_upper_bound)
+    return _min_cutoff, _max_cutoff, _stdev, _mean, _median, scores_per_lang 
 
-emoji_pattern = re.compile("["
-        u"\U0001F600-\U0001F64F"  # emoticons
-        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-                           "]+", flags=re.UNICODE)
 
 
   
