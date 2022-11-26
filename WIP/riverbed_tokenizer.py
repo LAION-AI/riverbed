45715e4338beee83b34ad0a0d5da5f6924a2e888
ontocord <ontocord@gmail.com>
2022-11-26 09:53:57 -0800

    Update and rename riverbed_tokenizer.py to WIP/riverbed_tokenizer.py

--- WIP/riverbed_tokenizer.py
+++ WIP/riverbed_tokenizer.py
@@ -40,8 +40,8 @@ import tqdm
 import gzip
 import multiprocessing
 from torch import nn
-from .utils import *
-from .searcher_indexer import *
+from ..utils import *
+from ..searcher_indexer import *
 
 if torch.cuda.is_available():
   device = 'cuda'
